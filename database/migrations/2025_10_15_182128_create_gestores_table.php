e8f90f9ad0b2540b350648b8530f9c59794f44b8
Bruno Ricardo de Menezes <brunomenezes1511@gmail.com>
2025-10-15 15:55:21 -0300

    feat: cria migrations para usuários, clientes e gestores

---
+++ database/migrations/2025_10_15_182128_create_gestores_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gestores', function (Blueprint $table) {
+            $table->id('id_gestor');
+            $table->string('nome', 100);
+            $table->string('email', 100)->unique();
+            $table->string('senha', 100);
+            $table->string('cnpj', 20);
+            $table->foreignId('usuario_id')->constrained('usuarios', 'id_usuario')->onDelete('cascade');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gestores');
+    }
+};
